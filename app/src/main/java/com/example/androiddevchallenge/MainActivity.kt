725d8f33622e2b23b0ccb82a80f376dfd739960e
Yogesh Paliyal <paliyalyogesh@gmail.com>
2021-03-04 23:36:55 +0530

    Basic Timer created

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -16,14 +16,30 @@
 package com.example.androiddevchallenge
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,8 +55,114 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
+
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        Timer()
+    }
+}
+
+@Composable
+fun Timer() {
+    val totalTimerTime = 180
+    var countdownSeconds by remember { mutableStateOf(totalTimerTime) }
+    var timerState by remember { mutableStateOf(0) } // 0 not started, 1 running, 2 is ended
+
+    when (timerState) {
+        0 -> {
+            ConstraintLayout(
+                Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+            ) {
+                val (progress, progressTxt, btnStart) = createRefs()
+
+                // not yet start show start button
+
+                Progress(
+                    Modifier.constrainAs(progress) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+                    countdownSeconds = countdownSeconds, totalSeconds = totalTimerTime
+                )
+
+                Button(
+                    modifier = Modifier.constrainAs(btnStart) {
+                        top.linkTo(progress.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+                    onClick = {
+                        countdownSeconds = totalTimerTime
+                        timerState = 1
+                    }
+                ) {
+                    Text(text = "Start")
+                }
+            }
+        }
+        1 -> {
+            // running show stop button
+
+            val minutes = countdownSeconds / 60
+            val seconds = countdownSeconds % 60
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Progress(countdownSeconds = countdownSeconds, totalSeconds = totalTimerTime)
+                TimerUi(minutes, seconds)
+            }
+
+            RunTimer(
+                time = countdownSeconds,
+                onTimerChange = {
+                    countdownSeconds = it
+                    if (it == 0) {
+                        // ended
+                        timerState = 2
+                    }
+                }
+            )
+        }
+        2 -> {
+            // timer stopped show restart button
+            Row(Modifier.fillMaxWidth()) {
+                Button(
+                    onClick = {
+                        timerState = 0
+                    }
+                ) {
+                    Text(text = "Reset")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Progress(modifier: Modifier = Modifier, countdownSeconds: Int, totalSeconds: Int) {
+    val progress = countdownSeconds.toFloat() / totalSeconds
+
+    CircularProgressIndicator(progress, modifier = modifier)
+}
+
+@Composable
+fun RunTimer(time: Int, onTimerChange: (Int) -> Unit) {
+    val coroutineScope = rememberCoroutineScope()
+    coroutineScope.launch {
+        Log.d("TestingLoop", "$time")
+        delay(1000)
+        var newTime = time
+        newTime--
+        onTimerChange(newTime)
+    }
+}
+
+@Composable
+fun TimerUi(minutes: Int, seconds: Int) {
+    Row(Modifier.fillMaxWidth()) {
+        Text(text = "$minutes")
+        Text(text = ":")
+        Text(text = "$seconds")
     }
 }
 
